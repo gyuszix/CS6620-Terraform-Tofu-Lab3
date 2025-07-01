7c143e4e770d8730bbd427b7724d178b18bbc7b6
gyuszix <gyuszix.gmail.com>
2025-07-01 15:48:13 -0400

    first commit

---
+++ exercise_1/variables.tf
@@ -0,0 +1,15 @@
+variable "file_name" {
+  default = "my_pets.txt"
+}
+
+variable "pet_prefix" {
+  default = "Mrs"
+}
+
+variable "pet_name_length" {
+  default = 3
+}
+
+variable "pet_name_seperator" {
+  default = "."
+}
